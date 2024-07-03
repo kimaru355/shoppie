8d5d652c1b771ccb3347c7c48a6c2de2069d1ba1
kimaru355 <emmanuelkimaru355@gmail.com>
2024-07-03 23:57:16 +0300

    add token in http headers

--- frontend/src/app/services/analytics.service.ts
+++ frontend/src/app/services/analytics.service.ts
@@ -1,17 +1,22 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Res } from '../interfaces/res';
 import { Analytic } from '../interfaces/analytic';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnalyticsService {
   api: string = 'http://localhost:3000/analytics';
-  constructor(private http: HttpClient) { }
+  headers = new HttpHeaders({
+    Authorization: localStorage.getItem('authToken') || '',
+  });
+  constructor(private http: HttpClient) {}
 
   getAnalytics(): Observable<Res<Analytic | null>> {
-    return this.http.get<Res<Analytic | null>>(this.api);
+    return this.http.get<Res<Analytic | null>>(this.api, {
+      headers: this.headers,
+    });
   }
 }
--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CartServices } from '../interfaces/cart_service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Cart, CartItem } from '../interfaces/cart';
 import { Res } from '../interfaces/res';
@@ -10,29 +10,44 @@ import { Res } from '../interfaces/res';
 })
 export class CartService implements CartServices {
   api: string = 'http://localhost:3000/cart';
+  headers = new HttpHeaders({
+    Authorization: localStorage.getItem('authToken') || '',
+  });
   constructor(private http: HttpClient) {}
 
   addToCart(item: CartItem): Observable<Res<null>> {
-    return this.http.post<Res<null>>(`${this.api}/add`, item);
+    return this.http.post<Res<null>>(`${this.api}/add`, item, {
+      headers: this.headers,
+    });
   }
 
   getCart(): Observable<Res<Cart[] | null>> {
-    return this.http.get<Res<Cart[] | null>>(`${this.api}/all`);
+    return this.http.get<Res<Cart[] | null>>(`${this.api}/all`, {
+      headers: this.headers,
+    });
   }
 
   deleteFromCart(id: string): Observable<Res<null>> {
-    return this.http.delete<Res<null>>(`${this.api}/delete/${id}`);
+    return this.http.delete<Res<null>>(`${this.api}/delete/${id}`, {
+      headers: this.headers,
+    });
   }
 
   emptyCart(): Observable<Res<null>> {
-    return this.http.delete<Res<null>>(`${this.api}/delete/all`);
+    return this.http.delete<Res<null>>(`${this.api}/delete/all`, {
+      headers: this.headers,
+    });
   }
 
   incrementCartItem(id: string): Observable<Res<null>> {
-    return this.http.put<Res<null>>(`${this.api}/increment/${id}`, null);
+    return this.http.put<Res<null>>(`${this.api}/increment/${id}`, {
+      headers: this.headers,
+    });
   }
 
   decrementCartItem(id: string): Observable<Res<null>> {
-    return this.http.put<Res<null>>(`${this.api}/decrement/${id}`, null);
+    return this.http.put<Res<null>>(`${this.api}/decrement/${id}`, {
+      headers: this.headers,
+    });
   }
 }
